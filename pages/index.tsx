13515f2d625b27ab0072a83949f4021c2f778d24
Ricardo de Arruda <settiricardo@gmail.com>
2020-05-01 03:47:15 +0300

    Fetch footer information from config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,46 @@
 import Home from '../components/home'
 import Social from '../components/social'
 import Layout from '../components/layout'
+import { GetStaticProps } from 'next'
+import { postsExist } from '../lib/postHelper'
+import { author, footerCopyright, baseURL } from '../config.json'
 
-export default function Index() {
+type Index = {
+  footer?: {
+    year: number,
+    author: string,
+    link: string,
+    copyRight: string,
+  }
+  hasPosts: boolean
+
+}
+
+export default function Index({ hasPosts, footer }: Index) {
   return (
-    <Layout title='Home'>
-    <Home>
-      <Social/>
-    </Home>
+    <Layout title='Home' footer={footer}>
+      <Home hasPosts={hasPosts}>
+        <Social />
+      </Home>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const dateNow = new Date()
+  const hasPosts = postsExist()
+  const footer = {
+    author: author.name,
+    copyRight: footerCopyright,
+    link: baseURL,
+    year: dateNow.getFullYear(),
+  }
+
+  return {
+    props: {
+      hasPosts,
+      footer
+    },
+  };
 }
