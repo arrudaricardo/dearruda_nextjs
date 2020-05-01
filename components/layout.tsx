e38e9870d319af293b3756792a47fb9c7e49f92b
Ricardo de Arruda <settiricardo@gmail.com>
2020-05-01 03:44:25 +0300

    Add footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,26 +2,50 @@ import Head from 'next/head'
 import { ReactElement } from 'react'
 import styles from '../styles/layout.module.css'
 
-type Props = {
+export type Props = {
+  footer?: {
+    year: number,
+    author: string,
+    link: string,
+    copyRight: string
+
+  },
   children: ReactElement,
   title: string
 }
 
-const Layout = ({children, title}: Props) => {
+const Layout = ({ children, title, footer }: Props) => {
   return (
-    <>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <main className={styles.main}>
-      {children}
-    </main>
-    <footer>{'I`m here to stay'}</footer>
-    </>
+    <div className={styles.body}>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <main className={styles.main}>
+        {children}
+      </main>
+      {footer &&
+        <footer className={styles.footer}>
+          <p className={styles.footerP}>
+            © {footer.year}
+            {" "}
+            <a href={footer.link}>{footer.author}</a>
+            {" · "}
+            {footer.copyRight === "CC" &&
+              <a href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank" rel="noopener">CC BY-NC 4.0</a>
+            }
+            {" · "}
+            <a href="/index.xml" target="_blank" title="rss">
+              <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" >
+                <path d="M4 11a9 9 0 0 1 9 9"> </path><path d="M4 4a16 16 0 0 1 16 16"></path> <circle cx="5" cy="19" r="1"></circle></svg></a>
+          </p>
+
+        </footer>
+      }
+    </div>
   )
 }
-
+//TODO: gen href for rss
 
 export default Layout
