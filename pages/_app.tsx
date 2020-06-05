675199bd44f952e6b7582cd7b44f71ff84366596
Ricardo de Arruda <settiricardo@gmail.com>
2020-06-06 01:47:51 +0300

    Add global prism js css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import {ReactPropTypes, JSXElementConstructor} from 'react'
 import '../styles/normalize.css' 
 import '../styles/global.css'
+import '../styles/prism.css'
 
 type Props = {
   Component: JSXElementConstructor<any>,
