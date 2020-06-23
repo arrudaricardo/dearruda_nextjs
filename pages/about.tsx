78792679d752ea2c3ed05e52b2f2224ac20b7567
Ricardo de Arruda <settiricardo@gmail.com>
2020-06-23 16:20:25 +0300

    add post style for about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,10 +2,21 @@ import { GetStaticProps} from 'next'
 import { readFileSync } from 'fs'
 import ReactMarkdown from 'react-markdown'
 import matter from 'gray-matter';
+import Layout from '../components/layout'
+import style from '../styles/post.module.css'
+import Footer from '../components/Footer'
+import { author, footerCopyright, baseURL } from '../config.json'
 
-const About = ({content}: {content: string}) => {
+const About = ({content, footer}: {content: string, footer: any}) => {
   return(
-    <ReactMarkdown source={content} />
+      <Layout title={"About"}>
+          <>
+              <div className={style.root}>
+                  <ReactMarkdown source={content} />
+          <Footer footer={footer} display='relative' />
+              </div>
+          </>
+      </Layout>
   )
 }
 
@@ -18,10 +29,19 @@ export const getStaticProps: GetStaticProps = async () => {
   const formattedDate = data.date.toLocaleDateString("en-US", options);
   const frontmatter = {...data, date: formattedDate}
 
+  const dateNow = new Date()
+  const footer = {
+    author: author.name,
+    copyRight: footerCopyright,
+    link: baseURL,
+    year: dateNow.getFullYear(),
+  }
+
   return (
     {
     props: {
       content,
+      footer,
       frontmatter
     }
   }
